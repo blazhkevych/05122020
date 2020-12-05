438373922cb2a07dee7e9675c8d9ee7ad7b19b99
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-12-05 21:28:40 +0200

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character ch = new Character();
+            ch.Order(2);
+            ch.Order(3);
+            ch.Order(1);
+            ch.Order(4);
+            Console.ReadKey();
+
+        }
+    }
+}
